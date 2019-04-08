b86b0bf01eb4e6bb39d7f9a417bd6971449f47dc
jzanderson09 <jzanderson09@gmail.com>
2019-04-07 20:37:41 -0600

    Finished functions.js.

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,11 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+//Higher order function:
+function consume (variable1, variable2, cb) {
+  return cb(variable1, variable2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +19,22 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add (num1, num2) {
+  return num1 + num2;
+}
+
+function multiply (num1, num2) {
+  return num1 * num2;
+}
+
+function greeting (str1, str2) {
+  return `Hello ${str1} ${str2}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,7 +42,8 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+// --> nestedfunction() can access the variable 'internal' because its lexical environment is its function-level/block-level scope and it's able to look 
+//outward for context.  nestedfunction wouldn't however be able to access the external variable because it's outside of its lexical scope.
 
 const external = "I'm outside the function";
 
